58184147a6fbfa8c281f545c8f71e4209b963463
PWCTW\JYKL <jackyleeidea@gmail.com>
2022-12-19 09:19:11 +0800

    PDF 浮水印版本v0.1.7

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,11 @@
       <div class="col">
         <!-- 第一步 -->
         <div class="card" style="margin-top:10px;">
-          <div class="card-header d-flex justify-content-between align-items-center"><b>Step1：請選擇欲添加浮水印的 PDF 檔</b></div>
+          <div class="card-header d-flex justify-content-between align-items-center"><b>Step1 : 請選擇欲添加浮水印之 PDF 檔</b>
+          </div>
           <div class="card-body" style="overflow-x:auto;">
             <label for="exampleFormControlInput1" class="form-label"><b>
-                <BIconFiles style="vertical-align:text-top;" class="icon" />本元件將指定 PDF 檔添加浮水印。
+                <BIconFiles style="vertical-align:text-top;" class="icon" />本元件會依據您的設定，於 PDF 檔中添加浮水印
               </b></label>
             <button type="button" class="btn" style="padding:0px;" v-on:click="help_1 = !help_1">
               <span v-if="!help_1">
@@ -31,21 +32,20 @@
             <br>
             <div v-if="help_1">
               <div class="mb-3" style="display:grid;justify-content:space-around;">
-                <label class="form-label"><b>1.可於元件前連結其他元件或直接選擇 PDF 檔案，若您是直接選擇 PDF 檔案則可跳過 2~3 步驟。</b></label>
+                <label class="form-label"><b>1-1 可於元件前連結其他元件（例如 : Directory ) 或直接選擇 PDF 檔案 ;<br>若直接選擇 PDF 檔案，請跳過 1-1 及
+                    1-2 步驟</b></label>
                 <img src="./step_1_1.png" style="width: 100%;max-width:650px;">
               </div>
               <div class="mb-3" style="display:grid;justify-content:space-around;">
-                <label
-                  class="form-label"><b>2.連結輸入資料後，因資料可能包含多個欄位，確認"檔案路徑"欄位。(下圖以資料夾工具為範例，其輸出結果有許多欄位，但只有路徑欄位才是我們需要的，故此處我們選擇
-                    fullpath 完整路徑欄位，提供後續轉置元件處理)</b></label>
+                <label class="form-label"><b>1-2 請確認輸入元件之"檔案路徑" 欄位 :<br>（下圖以Directory為例，請選擇 "FullPath" 完整路徑欄位，提供元件後續使用）</b></label>
                 <img src="./step_1_2.png" style="width: 100%;max-width:650px;">
               </div>
               <div class="mb-3" style="display:grid;justify-content:space-around;">
-                <label class="form-label"><b>3.選擇欄位。</b></label>
+                <label class="form-label"><b>1-3 請選擇 "檔案路徑" 欄位 </b></label>
                 <img src="./step_1_3.png" style="width: 100%;max-width:650px;">
               </div>
               <div class="mb-3" style="display:grid;justify-content:space-around;">
-                <label class="form-label"><b>若前面接的是 PDF 合併元件或 PDF 轉置元件，按同樣邏輯，我們選擇路徑欄位 Output Path 即可。</b></label>
+                <label class="form-label"><b>若前面接的是 PDF 合併元件或 PDF 轉置元件，請選擇 "Output Path" 路徑欄位</b></label>
                 <img src="./step_1_4.png" style="width: 100%;max-width:650px;">
               </div>
             </div>
@@ -54,7 +54,7 @@
             </ayx>
             <div class="mb-3">
               <label v-if="input_isConnectFile === true" for="exampleFormControlInput1" class="form-label"><b>
-                  <BIconColumns style="vertical-align:text-top;" class="icon" />根據您所連接的檔案，請選擇其輸出路徑 (Output) 欄位
+                  <BIconColumns style="vertical-align:text-top;" class="icon" />請依據您所連結之輸入元件，選擇 "檔案路徑" 欄位
                 </b></label>
               <select v-if="input_isConnectFile === true" class="form-control" v-model="connectInputPathMapping">
                 <option disabled value="">選擇欄位</option>
@@ -66,31 +66,40 @@
 
         <!-- 第二步 -->
         <div class="card" style="margin-top:10px;">
-          <div class="card-header d-flex justify-content-between align-items-center"><b>Step2：請輸入浮水印提示訊息</b></div>
+          <div class="card-header d-flex justify-content-between align-items-center"><b>Step2 : 請輸入浮水印提示訊息 </b></div>
           <div class="card-body" style="overflow-x:auto;">
             <div class="form-check form-check-inline mb-2">
               <input type="checkbox" class="form-check-input" v-model="input_isNeedAnnot" />
               <label for="exampleFormControlInput3" class="form-check-label"><b>
-                  添加頂部提示
+                  添加頂部訊息
                 </b></label>
             </div>
-            <div v-if="input_isNeedAnnot.toString()=='true'" class="mb-3">
+            <div v-if="input_isNeedAnnot.toString() == 'true'" class="mb-3">
               <div>
-              <BIconColumns style="vertical-align:text-top;" class="icon" /> 頂部提示</div>
+                <BIconColumns style="vertical-align:text-top;" class="icon" /> 頂部訊息
+              </div>
               <label for="exampleFormControlInput1" class="form-label"><b>
-                  <div>此內容將顯示於每一頁頂部提示訊息框，當前日期則使用"＠"代替。</div>
-                  <div class="mt-2">預設內容為 ：</div>
-                  <div>"<span style="color:orangered;">本資料係稿本@，僅供參考，不得移作其他用途。</span>"</div>
-                  <div>呈限於頂部提示框內容如下</div>
+                  <div>此訊息將顯示於每一頁頂部，欲顯示目前日期請使用"@"代替</div>
+                  <div class="mt-2">預設訊息為 :</div>
+                  <div>"<span style="color:orangered;">＂本資料係稿本＠，僅供參考，不得移作其他用途</span>"</div>
+                  <div>呈現於頂部訊息如下 :</div>
                   <img src="./step_2_1.png" style="width: 80%;max-width:650px;">
                 </b></label>
               <input type="text" id="exampleFormControlInput1" class="form-control" placeholder="輸入提示訊息"
                 v-model="input_annot">
             </div>
             <div>
-            <BIconColumns style="vertical-align:text-top;" class="icon" /> 浮水印
-            <div>PDF 頁面中間處添加 "DRAFT" 字樣浮水印，如下圖所示: </div>
-            <img src="./step_2_2.png" style="width: 80%;max-width:650px;"></div>
+              <div class="form-check form-check-inline mb-2">
+                <input type="checkbox" class="form-check-input" v-model="input_isNeedWatermark" />
+                <label for="exampleFormControlInput3" class="form-check-label"><b>
+                  添加 "DRAFT" 字樣浮水印
+                  </b></label>
+              </div>
+              <div v-if="input_isNeedWatermark.toString() == 'true'">
+                <div>PDF 頁面中間處添加 "DRAFT" 字樣浮水印，如下圖所示 : </div>
+                <img src="./step_2_2.png" style="width: 80%;max-width:650px;">
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -98,8 +107,7 @@
   </div>
 
   <footer class="footer mt-auto">
-    <p class="text-muted" style="margin: 0px;text-align: center;">版本：0.1.4</p>
-    <p class="text-muted" style="margin: 0px;text-align: center; font-size:10px;">Copyright © 2001-2022 Python Software Foundation; All Rights Reserved.</p>
+    <p class="text-muted" style="margin: 0px;text-align: center;">版本：0.1.7</p>
   </footer>
 
 </template>
@@ -144,16 +152,34 @@ export default {
       str_columns: [],
       val_columns: [],
       help_1: false,
-      input_isNeedAnnot: ""
+      input_isNeedAnnot: "",
+      input_isNeedWatermark:"",
+      last_incoming_fields:""
     }
   },
   components: {
   },
   watch: {
+    last_incoming_fields:{
+      handler(val) {
+        if (typeof window.Alteryx !== 'undefined') {
+          window.Alteryx.Gui.Manager.getDataItem("last_incoming_fields").setValue(val)
+        }
+      },
+      deep: true
+    },
     input_isNeedAnnot: {
       handler(val) {
         if (typeof window.Alteryx !== 'undefined') {
           window.Alteryx.Gui.Manager.getDataItem("input_isNeedAnnot").setValue(val)
+        }
+      },
+      deep: true
+    },
+    input_isNeedWatermark:{
+      handler(val) {
+        if (typeof window.Alteryx !== 'undefined') {
+          window.Alteryx.Gui.Manager.getDataItem("input_isNeedWatermark").setValue(val)
         }
       },
       deep: true
@@ -202,6 +228,10 @@ export default {
           manager.addDataItem(input_isConnectFile)
           var input_isNeedAnnot = new AlteryxDataItems.SimpleString('input_isNeedAnnot')
           manager.addDataItem(input_isNeedAnnot)
+          var input_isNeedWatermark = new AlteryxDataItems.SimpleString('input_isNeedWatermark')
+          manager.addDataItem(input_isNeedWatermark)
+          var last_incoming_fields = new AlteryxDataItems.SimpleString('last_incoming_fields')
+          manager.addDataItem(last_incoming_fields)
           var pdf_path = new AlteryxDataItems.SimpleString('pdf_path')
           manager.addDataItem(pdf_path)
           manager.bindDataItemToWidget(pdf_path, 'pdf_path')
@@ -222,6 +252,8 @@ export default {
           this.connectInputPathMapping = manager.getDataItem("connectInputPathMapping").getValue()
           this.input_isConnectFile = manager.getDataItem("input_isConnectFile").getValue()
           this.input_isNeedAnnot = manager.getDataItem("input_isNeedAnnot").getValue()
+          this.input_isNeedWatermark = manager.getDataItem("input_isNeedWatermark").getValue()
+          this.last_incoming_fields = manager.getDataItem("last_incoming_fields").getValue()
 
           // Load Income Field
           let str_type = ["String", "WString", "V_String", "V_WString", "Date", "Time", "DateTime"]
@@ -229,19 +261,25 @@ export default {
           let incomingFields = manager.getIncomingFields()
           this.str_columns = incomingFields.filter(item => str_type.indexOf(item.strType) > -1).map(item => item.strName)
           this.val_columns = incomingFields.filter(item => val_type.indexOf(item.strType) > -1).map(item => item.strName)
-          if (this.connectInputPathMapping.length==0) {
-            if(this.str_columns.includes("FullPath")){
+          // 若有更換連結檔案,則需自動抓取 Mapping 欄位
+          if (JSON.stringify(incomingFields) !== this.last_incoming_fields) {
+            if (this.str_columns.includes("FullPath")) {
               this.connectInputPathMapping = "FullPath"
             }
-            else if(this.str_columns.includes("Output Path")){
+            else if (this.str_columns.includes("Output Path")) {
               this.connectInputPathMapping = "Output Path"
             }
-            else{
+            else {
               this.connectInputPathMapping = ""
             }
           }
+          // 上次連結欄位
+          this.last_incoming_fields = JSON.stringify(incomingFields)
           if (this.input_isNeedAnnot === "") {
             this.input_isNeedAnnot = true
+          }
+          if(this.input_isNeedWatermark === ""){
+            this.input_isNeedWatermark = true
           }
           if ((this.str_columns.length + this.val_columns.length) === 0) {
             this.input_isConnectFile = false;
